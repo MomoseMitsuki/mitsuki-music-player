74788308561fd776d1bd03fef9dbc9270146906c
百濑美月 <15230787+MomoseMitsuki@user.noreply.gitee.com>
2026-05-21 00:31:09 +0800

    feat(web): 添加nuxt module, 标题调整, scss注入, 自动导入

--- packages/web/nuxt.config.ts
+++ packages/web/nuxt.config.ts
@@ -1,3 +1,7 @@
+import AutoImport from "unplugin-auto-import/vite";
+import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
+import Components from "unplugin-vue-components/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	compatibilityDate: "2025-07-15",
@@ -7,11 +11,63 @@ export default defineNuxtConfig({
 		layoutTransition: { name: "page", mode: "out-in" },
 
 		// https://github.com/nuxt/nuxt/issues/26565#issuecomment-3448517709
-		baseURL: "/"
+		baseURL: "/",
+		head: {
+			titleTemplate: "%s - 『 花 逢 坂 町 』 🌸 音乐演绎厅"
+		}
 	},
-	modules: ["nuxtjs-naive-ui", "@nuxt/image", "@pinia/nuxt", "@nuxtjs/seo"],
+	imports: {
+		dirs: ["stores", "views"],
+		scan: true
+	},
+	vite: {
+		plugins: [
+			AutoImport({
+				imports: [
+					{
+						"naive-ui": [
+							"useDialog",
+							"useMessage",
+							"useNotification",
+							"useLoadingBar"
+						]
+					}
+				]
+			}),
+			Components({
+				resolvers: [NaiveUiResolver()]
+			})
+		],
+		optimizeDeps: {
+			include: ["@css-render/vue3-ssr"]
+		},
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: `
+                        @use "~/assets/styles/index.scss" as *;
+                    `
+				}
+			}
+		}
+	},
+	modules: [
+		"@nuxt/image",
+		"@pinia/nuxt",
+		// "@nuxtjs/seo",
+		"@nuxt/icon"
+	],
 	devServer: {
 		host: "127.0.0.1",
 		port: 2333
+	},
+	build: {
+		transpile: ["naive-ui", "vueuc"]
+	},
+	// ogImage: {
+	// 	enabled: false
+	// },
+	icon: {
+		mode: "svg"
 	}
 });
