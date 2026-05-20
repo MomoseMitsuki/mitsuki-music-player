05dd8e8fb343ee127de1cf7be0ae6ea9c6934918
百濑美月 <15230787+MomoseMitsuki@user.noreply.gitee.com>
2026-05-21 00:41:53 +0800

    feat(web): 一级导航组件, 搜索栏, 视频卡片组件

---
+++ packages/web/app/components/NavItem.vue
@@ -0,0 +1,60 @@
+<template>
+	<NuxtLink :to="to" class="nav__item">
+		<Icon :name="icon"></Icon>
+		<div class="text">
+			<div class="name">{{ name }}</div>
+			<div class="ename">{{ ename }}</div>
+		</div>
+	</NuxtLink>
+</template>
+
+<script lang="ts" setup>
+import type { RouteLocationRaw } from "vue-router";
+
+defineProps<{
+	to: RouteLocationRaw;
+	icon: string;
+	name: string;
+	ename: string;
+}>();
+</script>
+
+<style scoped lang="scss">
+$nav_item_height: 60px;
+
+.nav__item {
+	width: 80%;
+	display: flex;
+	align-items: center;
+	text-decoration: none;
+	height: $nav_item_height;
+	line-height: $nav_item_height;
+	margin: auto;
+	border-radius: 10px;
+	color: $text_color;
+	padding: 0 18px;
+	user-select: none;
+	&:hover {
+		color: $primary;
+		background: linear-gradient(to right, #f7e2f4 0%, #f9f3fa 100%);
+	}
+}
+
+.text {
+	text-align: start;
+	margin: 0 15px;
+	.name {
+		line-height: 14px;
+		font-size: 10px;
+	}
+	.ename {
+		line-height: 11px;
+		font-size: 7px;
+	}
+}
+svg {
+	display: block;
+	width: 25px;
+	height: 25px;
+}
+</style>
---
+++ packages/web/app/components/VideoCard.vue
@@ -0,0 +1,65 @@
+<script lang="ts" setup>
+defineProps<{
+	src: string;
+	title: string;
+	singer: string;
+	duration: number;
+	view: number;
+	updateTime: number;
+}>();
+</script>
+
+<template>
+	<div class="card__container">
+		<div class="img__container">
+			<NuxtImg
+				:src="src"
+				placeholder="/images/default_video_avatar.webp"
+				width="216px"
+				height="159px"
+				fit="cover"
+			/>
+			<div>{{ formatTime(duration) }}</div>
+		</div>
+		<div class="info__container">
+			<div class="title">{{ title }}</div>
+			<div>{{ singer }}</div>
+			<div>{{ view }}次观看 {{ formatTimeAgo(updateTime) }}</div>
+		</div>
+	</div>
+</template>
+
+<style scoped lang="scss">
+.card__container {
+	width: 216px;
+	border-radius: 10px;
+	user-select: none;
+	cursor: pointer;
+	.img__container {
+		position: relative;
+		transform-origin: center center;
+		transition: transform 0.3s ease;
+		&:hover {
+			transform: scale(1.03);
+		}
+		img {
+			border-radius: 10px;
+		}
+		div {
+			color: white;
+			position: absolute;
+			right: 10px;
+			bottom: 10px;
+		}
+	}
+}
+.info__container {
+	padding: 0px 10px;
+	font-size: 11px;
+	.title {
+		font-weight: bolder;
+		font-size: 15px;
+		color: $text_color;
+	}
+}
+</style>
