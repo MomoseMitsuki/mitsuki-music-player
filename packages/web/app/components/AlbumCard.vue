f4d2b0dd9844f7b76c4ba632036e3aff8afeb4b0
百濑美月 <15230787+MomoseMitsuki@user.noreply.gitee.com>
2026-05-21 00:33:18 +0800

    feat(web): 专辑卡片

---
+++ packages/web/app/components/AlbumCard.vue
@@ -0,0 +1,82 @@
+<script lang="ts" setup>
+defineProps<{
+	src: string;
+	title: string;
+	subTitle: string;
+	date: string;
+}>();
+</script>
+
+<template>
+	<div class="card__container">
+		<div style="position: relative">
+			<NuxtImg
+				:src="src ? src : undefined"
+				placeholder="/images/default_music_avatar.webp"
+				width="180px"
+				height="170px"
+				fit="cover"
+			/>
+			<div class="play__btn">
+				<Icon name="mdi:play" size="20" />
+			</div>
+		</div>
+		<div class="info__container">
+			<h5>{{ title }}</h5>
+			<div>{{ subTitle }}</div>
+			<div>{{ date }}</div>
+		</div>
+	</div>
+</template>
+
+<style scoped lang="scss">
+@use "sass:math" as math;
+.card__container {
+	width: 180px;
+	height: 245px;
+	border-radius: 10px;
+	overflow: hidden;
+	box-shadow:
+		3px 0 10px #ece9f1,
+		0 3px 10px #ece9f1,
+		-3px 0 10px #ece9f1,
+		0 -3px 10px #ece9f1;
+	transition: transform 0.3s ease;
+	user-select: none;
+	cursor: pointer;
+	&:hover {
+		transform: translateY(-5px);
+		box-shadow:
+			6px 0 15px #ece9f1,
+			0 6px 15px #ece9f1,
+			-6px 0 15px #ece9f1,
+			0 -6px 15px #ece9f1;
+	}
+}
+.info__container {
+	padding: 5px 20px;
+	h5 {
+		color: $text_color;
+	}
+	div {
+		font-size: 10px;
+		color: $light_text_color;
+	}
+}
+.play__btn {
+	$w: 30px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	position: absolute;
+	right: 10px;
+	bottom: 15px;
+	background-color: #f9f6f8;
+	width: $w;
+	height: $w;
+	border-radius: math.div($w, 2);
+	svg {
+		color: $primary;
+	}
+}
+</style>
