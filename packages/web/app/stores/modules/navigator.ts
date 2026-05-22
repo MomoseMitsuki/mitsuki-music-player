2373c4639d6006f1fdeaf43b7e4bae062bb889fe
百濑美月 <15230787+MomoseMitsuki@user.noreply.gitee.com>
2026-05-22 10:13:48 +0800

    fix(web): 将navigate跳转函数放入pinia单例仓库

---
+++ packages/web/app/stores/modules/navigator.ts
@@ -0,0 +1,28 @@
+type Id = string | number;
+
+export const useNavigatorStore = defineStore("navigatorStore", () => {
+	const router = useRouter();
+
+	function navigateVideo(id: Id) {
+		router.push(`/video/${id}`);
+	}
+
+	function navigatePlaylist(id: Id) {
+		router.push(`/playlist/${id}`);
+	}
+
+	function navigateAlbum(id: Id) {
+		router.push(`/album/${id}`)
+	}
+
+	function navigateArtist(id: Id) {
+		router.push(`/artist/${id}`)
+	}
+
+	return {
+		navigateVideo,
+		navigatePlaylist,
+		navigateAlbum,
+		navigateArtist
+	};
+})
