b3a23e7e7556658663e6da832ee875c474e87c20
MomoseMitsuki <2361902570@qq.com>
2026-06-10 14:29:22 +0800

    feat(web): 权限级插槽组件

---
+++ packages/web/app/components/hoc/OwnerOnly.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+const props = defineProps<{
+	id: string;
+}>();
+
+const userStore = useUserStore();
+const { user } = storeToRefs(userStore);
+
+const isOwner = computed(() => {
+	return user.value && user.value.id === props.id;
+});
+</script>
+
+<template>
+	<slot v-if="isOwner"></slot>
+	<slot v-else name="fallback"></slot>
+</template>
